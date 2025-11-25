576c46baf099b28dc0cbccae27e9b368ce7e8337
Kim K <dickykam3@gmail.com>
2025-11-25 23:51:35 +0800

    chore: update openai_chatgpt.py with example of semantic dilution

--- examples/adapter/openai_chatgpt.py
+++ examples/adapter/openai_chatgpt.py
@@ -4,14 +4,49 @@ from gptcache.adapter import openai
 cache.init()
 cache.set_openai_key()
 
-question = 'what is github'
 answer = openai.ChatCompletion.create(
       model='gpt-3.5-turbo',
       messages=[
         {
             'role': 'user',
-            'content': question
+            'content': 'milk is good for your health'
+        },
+        {
+            'role': 'assistant',
+            'content': 'yes'
+        },
+        {
+            'role': 'user',
+            'content': 'milk is bad for your health'
+        },
+        {
+            'role': 'assistant',
+            'content': 'yes'
+        },
+        {
+            'role': 'user',
+            'content': 'list more reasons'
         }
+      ],
+    )
+
+print(answer)
+
+answer = openai.ChatCompletion.create(
+      model='gpt-3.5-turbo',
+      messages=[
+        {
+            'role': 'user',
+            'content': 'milk is both good and bad for your health'
+        },
+        {
+            'role': 'assistant',
+            'content': 'yes'
+        },
+        {
+            'role': 'user',
+            'content': 'list more reasons'
+        },
       ],
     )
 print(answer)
